f0f25225e4a3c53cacc70ca84f896f82b4199a68
Don Mamaril <mamarildon@gmail.com>
2014-05-22 19:16:43 -0700

    Toggle signup form & redirects to /account on login

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,20 +1,31 @@
 // angular.module('HomeCtrl', []).controller('HomeController', function($scope, $http, $window) {
 
-app.controller('HomeController', function ($scope, $http, $window) {
+app.controller('HomeController', function ($scope, $http, $window, $location) {
   $scope.tagline = 'KillBnB';
+  $scope.showSignUp = false;
+
+  $scope.toggleSignUp = function () {
+    $scope.showSignUp = true;
+  }
 
   $scope.logInUser = function () {
     $http.post('/login', { email: $scope.email, password: $scope.password })
       .success(function (token, status, headres, config) {
-        console.log(token);
         $window.sessionStorage.token = token.token;
         $window.sessionStorage.id = token._id;
         $window.sessionStorage.name = token.name;
+        $location.path('/account')
       });
   };
 
   $scope.registerUser = function () {
-    $http.post('/register', { email: $scope.email, password: $scope.password, name: $scope.name});
+    var userData = {
+      email: $scope.email, 
+      password: $scope.password, 
+      name: $scope.name
+    };
+    // need to receive session token & assign to $window.sessionStorage then redirect to /account
+    $http.post('/register', userData);
   };
 })
 
