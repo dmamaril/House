9b4f3b2b930bc07697cd35e67f628be1b846318e
Vin Halbwachs <vhalbwachs@gmail.com>
2014-05-25 13:28:44 -0700

    Basic google maps integration

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	// url : 'mongodb://<user>:<pass>@mongo.onmodulus.net:27017/uw45mypu'
-  url: 'mongodb://localhost/house'
+  url: 'mongodb://housedev:password@ds033449.mongolab.com:33449/housedb'
 }
--- public/js/controllers/ListCtrl.js
+++ public/js/controllers/ListCtrl.js
@@ -5,11 +5,21 @@ app.controller('ListController', function ($scope, List, properties, $window) {
   
   $scope.map = {
       center: {
-          latitude: 45,
-          longitude: -73
+          latitude: 45.5,
+          longitude: -73.5
       },
       zoom: 13
   };
+
+  var div = '<h4 class="list-group-item-heading">Your next shitty apartment</h4>' + 
+      '<p class="list-group-item-text">This is a sample of the div you can have. This could be your next shitty apartment!</p>';
+  $scope.map.markers = [
+    {coords: {latitude: 45.5, longitude: -73.5}, show:false, info: {price: 1000, title: 'Whatever'}},
+    {coords: {latitude: 45, longitude: -73.5}, show:false, info: {price: 1500, title: 'Whatever2'}},
+    {coords: {latitude: 45.5, longitude: -73}, show:false, info: {price: 1200, title: 'Whatever3'}}
+  ];
+
+
 
   $scope.toggleGroupForm = function () {
     $scope.creatingGroup = !$scope.creatingGroup;
@@ -31,8 +41,8 @@ app.controller('ListController', function ($scope, List, properties, $window) {
     delete $window.sessionStorage.token;
     delete $window.sessionStorage.id;
     delete $window.sessionStorage.name;
+
+  console.log(div);
   };
 
 });
-
-
